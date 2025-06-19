196a623c9dd5bc32ed1bdaf9a40f58f00d5b7108
AMIRFAHAD09 <fahadkhan30145@gmail.com>
2025-06-19 12:00:33 +0530

    add

--- Controller/upload-controller.js
+++ Controller/upload-controller.js
@@ -3,7 +3,9 @@ const User = require('../models/auth-model');
 const uploadProfilePic = async (req, res) => {
   try {
     const userId = req.params.id;
-    const imagePath = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    
+    const BASE_URL = process.env.BASE_URL || `${req.protocol}://${req.get("host")}`;
+    const imagePath = `${BASE_URL}/uploads/${req.file.filename}`;
 
     const updatedUser = await User.findByIdAndUpdate(
       userId,
