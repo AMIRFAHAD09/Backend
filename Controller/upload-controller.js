7d1c756f062cdbeb68489095b34dde42ba68e0b5
AMIRFAHAD09 <fahadkhan30145@gmail.com>
2025-06-19 11:35:31 +0530

    add picture solution

--- Controller/upload-controller.js
+++ Controller/upload-controller.js
@@ -3,7 +3,7 @@ const User = require('../models/auth-model');
 const uploadProfilePic = async (req, res) => {
   try {
     const userId = req.params.id;
-    const imagePath = `http://localhost:5000/uploads/${req.file.filename}`;
+    const imagePath = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
 
     const updatedUser = await User.findByIdAndUpdate(
       userId,
